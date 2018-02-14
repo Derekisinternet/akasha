a9fb59357c9290270316915a9dfa477c59caa32c
Derek <derekisinternet@gmail.com>
2018-02-14 16:02:16 +0000

    tweaked inventory setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # akasha
 Ansible repo
+
+## Inventory
+The setup script creates an `inventory` directory in the repo's root folder. This folder is not included in git for security reasons.
--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@
 pip install ansible
 pip install 'docker-py>=1.7.0'
 
-#now that ansible is installed:
+# now that ansible is installed:
 ansible-galaxy install geerlingguy.jenkins -p ./roles/
+
+# create inventory file:
+mkdir inventory
+touch inventory/main.yml
