fdaadcb49ea36668b13a2081ee5840407073a203
Derek <derekisinternet@gmail.com>
2018-03-16 23:56:49 -0700

    added virtualenv

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pip install ansible
+sudo pip install pipenv
+pipenv --python 2.7
+pipenv install 
 
 #now that ansible is installed:
 ansible-galaxy install geerlingguy.jenkins -p ./roles/
